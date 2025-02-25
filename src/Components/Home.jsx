c1305f4a5e9dffbd77edc40bcd0f31072dfd17ed
DONAELIZABETH <127596915+DONAELIZABETH@users.noreply.github.com>
2025-02-25 16:30:41 +0530

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -11,13 +11,13 @@ const Home = () => {
     <div className="home">
       <div className="overlay"></div>
       <div className="content">
-        <img className="home-img" src="/img2.jpg" alt="Dona Elizabeth Saji" />
+        <img className="home-img" src="/img d.jpg" alt="Dona Elizabeth Saji" />
         <div className="head">
           <h3 className="home-heading3">{add}</h3>
           <h1 className="home-heading1">{mail}</h1>
           <h2 className="home-heading2">{num}</h2>
           <div className="buttons">
-            <Link to="/Cv.pdf" target="_blank" download>
+            <Link to="/dona new.pdf" target="_blank" download>
               <button className="downloadcv-button">Download CV</button>
             </Link>
             <Link to="/contact">
